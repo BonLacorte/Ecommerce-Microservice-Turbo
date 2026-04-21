e336856ab31156f8b6cb16500a32ff9f64854307
Bon Lacorte <bonbonlacorte123@gmail.com>
2026-04-22 06:49:48 +0800

    changes in layouts

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -17,8 +17,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Trendlama - Best Clothes",
-  description: "Trendlama is the best place to find the best clothes",
+  title: "TrendCosta - Best Clothes",
+  description: "TrendCosta is the best place to find the best clothes",
 };
 
 export default function RootLayout({
